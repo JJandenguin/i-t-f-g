3944f73c383e6bb4555bef8f2650b0b6d05e9efe
JJ Cox <jaydencox1029@att.net>
2024-12-14 19:36:09 -0700

    Update auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -1,14 +1,22 @@
-sudo apt update && upgrade -y
-sudo apt install flatpak
+sudo apt update && sudo apt upgrade -y
+sudo apt install flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.brave.Browser
-flatpak install flathub net.lutris.Lutris
-flatpak install flathub com.heroicgameslauncher.hgl
-flatpak install flathub com.valvesoftware.Steam
-flatpak install flathub com.usebottles.bottles
-flatpak run com.brave.Browser
-flatpak run net.lutris.Lutris
-flatpak run com.heroicgameslauncher.hgl
-flatpak run com.valvesoftware.Steam
-flatpak run com.usebottles.bottles
-exit 1
+
+if [ ! -f /tmp/restart_marker ]; then
+    touch /tmp/restart_marker
+    sudo reboot
+    exit 0
+fi
+
+rm -f /tmp/restart_marker
+flatpak install -y flathub com.brave.Browser
+flatpak install -y flathub net.lutris.Lutris
+flatpak install -y flathub com.heroicgameslauncher.hgl
+flatpak install -y flathub com.valvesoftware.Steam
+flatpak install -y flathub com.usebottles.bottles
+flatpak run com.brave.Browser &
+flatpak run net.lutris.Lutris &
+flatpak run com.heroicgameslauncher.hgl &
+flatpak run com.valvesoftware.Steam &
+flatpak run com.usebottles.bottles &
+exit 0
