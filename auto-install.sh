76f42ede8fdc90ce3454e4055dca16176da6c06d
JJ Cox <jaydencox1029@att.net>
2024-12-14 19:22:53 -0700

    Update auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -1,26 +1,10 @@
 echo "updating and upgrading"
 sudo apt update && upgrade -y
-
-echo "installing faltpak"
+sudo apt list --upgradable
 sudo apt install flatpak
-
-echo "installing brave"
 flatpak run com.brave.Browser
-
-echo "installing lutris"
 flatpak run net.lutris.Lutris
-
-echo "installing heroic"
 flatpak run com.heroicgameslauncher.hgl
-
-echo "installing steam"
 flatpak run com.valvesoftware.Steam
-
-echo "installing bottels"
 flatpak run com.usebottles.bottles
-
-echo "ensuring everything is up to date..."
-sudo apt update && upgrade -y
-
-echo "install sucessful!... now exiting"
 exit 1
