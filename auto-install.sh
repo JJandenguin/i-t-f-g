832262a94ebb8229fdd606a8f483a108b89b2e33
JJ Cox <jaydencox1029@att.net>
2024-12-14 19:31:54 -0700

    Update auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -1,5 +1,6 @@
 sudo apt update && upgrade -y
 sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.brave.Browser
 flatpak install flathub net.lutris.Lutris
 flatpak install flathub com.heroicgameslauncher.hgl
