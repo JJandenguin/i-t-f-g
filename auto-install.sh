08c874b1f7dc1226627103ed008059aca3dff2fe
JJ Cox <jaydencox1029@att.net>
2024-12-14 19:29:44 -0700

    Update auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -1,13 +1,13 @@
 sudo apt update && upgrade -y
 sudo apt install flatpak
 flatpak install flathub com.brave.Browser
-flatpak run com.brave.Browser
 flatpak install flathub net.lutris.Lutris
-flatpak run net.lutris.Lutris
 flatpak install flathub com.heroicgameslauncher.hgl
-flatpak run com.heroicgameslauncher.hgl
 flatpak install flathub com.valvesoftware.Steam
-flatpak run com.valvesoftware.Steam
 flatpak install flathub com.usebottles.bottles
+flatpak run com.brave.Browser
+flatpak run net.lutris.Lutris
+flatpak run com.heroicgameslauncher.hgl
+flatpak run com.valvesoftware.Steam
 flatpak run com.usebottles.bottles
 exit 1
